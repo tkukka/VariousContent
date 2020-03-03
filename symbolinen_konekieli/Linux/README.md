3ff7790862fc0c98b199f7122e7b490f44499861
Tero Kukka <tkukka@users.noreply.github.com>
2020-03-03 15:32:20 +0200

    Linux: 32-bittinen asm-koodi.

---
+++ symbolinen_konekieli/Linux/README.md
@@ -0,0 +1,21 @@
+# Yleistä
+
+Ubuntu 18.04 LTS (64-bit)  
+Netwide Assembler [NASM](https://nasm.us/ "nasm") v. 2.14.02 Stable  
+Linkkeri: GNU ld (GNU Binutils for Ubuntu) 2.30  
+[The CDT stand-alone debugger](https://www.eclipse.org/cdt/downloads.php)  
+
+### Dokumentit  
+
+[Sample nasm programs](https://www.csee.umbc.edu/portal/help/nasm/sample.shtml )  
+
+[Agner Fog blogi](https://www.agner.org/optimize/)  
+[System V Application Binary Interface] (https://github.com/hjl-tools/x86-psABI/wiki/X86-psABI)  
+[AMD](https://www.amd.com/en/support/tech-docs)  
+[Intel](https://software.intel.com/en-us/articles/intel-sdm)
+
+
+# 32-bittinen kehitys 64-bittisellä alustalla
+
+Systeemikutsujen numerot: /usr/include/x86_64-linux-gnu/asm/unistd_32.h  
+Paketit: gcc-multilib libc6-dev-i386  
---
+++ symbolinen_konekieli/Linux/eka_32.asm
@@ -0,0 +1,35 @@
+                            BITS 32
+
+laitetulostus               EQU     4
+stdout                      EQU     1
+loppu_hyvin                 EQU     0
+lopetus                     EQU     1
+
+                            SECTION .data
+
+viesti                      db      "Ääkköset äöåÄÖÅ. Хорошо Müller Straße",10,0
+viestin_pituus              EQU     $ - viesti
+
+toinen_viesti               db      "Toinenkin tuloste.",10,0
+toisen_viestin_pituus       EQU     $ - toinen_viesti
+
+                            SECTION .text
+
+                            GLOBAL _start
+
+_start:                     mov     edx, viestin_pituus
+                            mov     ecx, viesti
+                            mov     ebx, stdout
+                            mov     eax, laitetulostus
+                            int     80h                         ;vanhempi keino
+
+                            mov     edx, toisen_viestin_pituus
+                            mov     ecx, toinen_viesti
+                            mov     ebx, stdout
+                            mov     eax, laitetulostus
+                            int     80h
+
+                            mov     ebx, loppu_hyvin
+                            mov     eax, lopetus
+                            int     80h
+
