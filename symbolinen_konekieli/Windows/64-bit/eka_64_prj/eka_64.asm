81a617e5f3fed3f3d0e6a2940be50a40cde62dfa
Tero Kukka <tkukka@users.noreply.github.com>
2020-04-05 19:52:53 +0300

    Win x64 asm ja tekstin tulostus konsoliin (ANSI CP1252).

--- symbolinen_konekieli/Windows/64-bit/eka_64_prj/eka_64.asm
+++ symbolinen_konekieli/Windows/64-bit/eka_64_prj/eka_64.asm
@@ -1,12 +1,49 @@
+; Visual Studio koodisivu 1252
+                            EXTERN  GetStdHandle:proc
+                            EXTERN  WriteConsoleA:proc          ;ANSI
                             EXTERN  ExitProcess:proc
 
+STD_OUTPUT_HANDLE           EQU     -11
+INVALID_HANDLE_VALUE        EQU     -1
+
+                            .CONST
+
+teksti                      DB      'Testitulostus.', 13, 10
+tekstin_pituus              EQU     ($ - teksti)
+teksti2                     DB      'Ääkköset åäöÅÄÖ. Müller. Straße.', 13, 10
+teksti2_pituus              EQU     ($ - teksti2)
+
                             .DATA
+
+hConsole                    QWORD   ?
 
                             .CODE
 
 start                       PROC
-                            sub     rsp, 32                         ;varataan pinosta muistia 4*8 tavua
-                            sub     rcx, rcx
+                            sub     rsp, 5*8                    ;varataan pinosta muistia (tavuina) kutsuttavia funktioita varten.
+                            mov     rcx, STD_OUTPUT_HANDLE      ;Windows x64: 4 ensimmäistä rekisteriin RCX, RDX, R8, R9
+                            call    GetStdHandle
+                            cmp     rax, 0
+                            je      lopeta
+                            cmp     rax, INVALID_HANDLE_VALUE
+                            je      lopeta
+                            mov     [hConsole], rax             ;tallennetaan konsolin kahva
+; Aloitetaan tulostukset
+                            mov     rcx, [hConsole]
+                            lea     rdx, teksti
+                            mov     r8, tekstin_pituus
+                            mov     r9, 0                       ;kirjoitettujen merkkien määrä ei kiinnosta
+                            mov     QWORD PTR [rsp + 32], 0
+                            call    WriteConsoleA
+                            cmp     rax, 0
+                            je      lopeta                      ;tapahtui virhe
+                            mov     rcx, [hConsole]
+                            lea     rdx, teksti2
+                            mov     r8, teksti2_pituus
+                            mov     r9, 0
+                            mov     QWORD PTR [rsp + 32], 0
+                            call    WriteConsoleA
+lopeta:                     sub     rcx, rcx
                             call    ExitProcess
 start                       ENDP
                             END
--- symbolinen_konekieli/Windows/README.md
+++ symbolinen_konekieli/Windows/README.md
@@ -9,17 +9,16 @@ Visual Studio 2019
 ### Dokumentit  
 
 [Microsoft Macro Assembler reference](https://docs.microsoft.com/en-us/cpp/assembler/masm/microsoft-macro-assembler-reference)  
-[x64 software conventions](https://docs.microsoft.com/en-us/cpp/build/x64-software-conventions)  
-[x64 calling convention](https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention)  
+[Microsoft Macro Assembler (MASM) Unofficial Changelist](http://bytepointer.com/masm/index.htm)  
 [Agner Fog blogi](https://www.agner.org/optimize/)  
 [AMD](https://www.amd.com/en/support/tech-docs)  
-[Intel](https://software.intel.com/en-us/articles/intel-sdm)
-
-# 32-bittinen kehitys 64-bittisellä alustalla
+[Intel](https://software.intel.com/en-us/articles/intel-sdm)  
 
 # 64-bittinen kehitys 64-bittisellä alustalla
 
+[x64 software conventions](https://docs.microsoft.com/en-us/cpp/build/x64-software-conventions)  
+[x64 calling convention](https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention)  
 
 ### eka_64.asm
 
-Tulostetaan tekstiä
+Tulostetaan tekstiä WriteConsoleA-funktiota käyttäen (ANSI teksti, koodisivu 1252). Konsoliin komento: ```chcp 1252```.
