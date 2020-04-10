4ee98d5ab50296e94c3fbb0f297c22ea202e57bc
Tero Kukka <tkukka@users.noreply.github.com>
2020-04-10 13:50:55 +0300

    Robustimpi pinon käsittely EBP:n kautta.

--- symbolinen_konekieli/Linux/kuudes_sse_32.asm
+++ symbolinen_konekieli/Linux/kuudes_sse_32.asm
@@ -14,16 +14,15 @@ vakio_pii                   dq      3.141592654             ;const double pii = 
 ;vastaa: double laske_ala(double). xmm0 = ympyrän pinta-ala. Siirretään lopuksi ala FPU:n st0-rekisteriin (Sys V 32-bit ABI)
 laske_ala:                  push    ebp                     ;ABI: rekisterin tila säilytettävä
                             mov     ebp, esp                ;funktion pinokehys
-                            sub     esp, 16                 ;double välikaisesti pinoon tähän. 16 tavua pitäisi riittää.
+                            sub     esp, 8                  ;pinomuistin varaus. double (8 tavua) välikaisesti pinoon.
                             movsd   xmm0, [ebp + 8]         ;säde pinosta
                             mulsd   xmm0, xmm0              ;xmm0 = säde²
                             mulsd   xmm0, [vakio_pii]       ;π otetaan muistivakioista, xmm0 = π * säde²
-                            movsd   [esp], xmm0             ;pinoon
-                            fld     qword [esp]             ;st0:aan
-                            add     esp, 16                 ;väliaikaisen muistin vapautus
+                            movsd   [ebp - 8], xmm0         ;pinoon
+                            fld     qword [ebp - 8]         ;st0:aan
                             push    metodi
                             call    set_method              ;kerrotaan main():iin laskentatapa
                             add     esp, 4
-                            leave                           ;funktion pinon kehyksen purku
+                            leave                           ;funktion pinon kehyksen purku ja väliaikaisen muistin vapautus
                             ret
 
